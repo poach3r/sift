f880db8b655e1a1751de61621b936932db7b8156
poacher <58641438+poach3r@users.noreply.github.com>
2023-03-02 14:06:38 -0500

    Add files via upload

--- sift.sh
+++ sift.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 first=$(echo $1 | head -c 1) #set to first letter of inputted package
-url=http://ftp.us.debian.org/debian/pool/main/$first/$1/ #set to ftp url, to change mirrors change to a url from https://packages.debian.org/bullseye/amd64/i3-wm/download
+url=http://ftp.us.debian.org/debian/pool/main/$first/$1/ #set to ftp url | to change mirror locaton, change ftp.us.debian.org to one from here https://packages.debian.org/bullseye/all/neofetch/download
+if [ $(w3m $url | grep found | awk '{ print$1 }') == "The" ]
+then
+    echo "'$1' Not Found"
+    exit
+fi
 w3m $url | grep .deb | awk '{ print$3 }' #display available packages
 read -p "Enter Package Name > " pacname #read real package name
 wget -O /tmp/package.deb $url/$pacname #download deb package to /tmp/
 xdeb -Sde /tmp/package.deb #convert deb package to void package
-xbps-install -R binpkgs $(xdeb -Sde /tmp/package.deb | awk '{print $7}' | sed 's/.....$//' | tail -n 1) #install package
-rm -f /tmp/package.deb #delete deb package
+xbps-install -R binpkgs $(xdeb -Sde /tmp/package.deb | awk '{print $7}' | tr -d '`' | sed 's/\`//g' | tail -n 1) #install created void package, doesnt work!
+rm /tmp/package.deb #delete deb package
 
-echo "Done!"s
-  
+echo "Done!"
